208224d32b5423729427c7ebc921349da1666c34
stasiandr <stasiandr@gmail.com>
2015-02-08 16:04:07 +0300

    last updates

--- js/main3.js
+++ js/main3.js
@@ -15,10 +15,61 @@ $(window).bind('scroll', function(){
 });
 
 $( "#port" ).click(function() {
-  $( ".port_gal" ).animate({
+  $( "#port_gal" ).animate({
+    height:"toggle"
+  }, 500, function() {});
+});
+$( "#na" ).click(function() {
+  $( "#na_gal" ).animate({
+    height:"toggle"
+  }, 500, function() {});
+});
+$( "#pe" ).click(function() {
+  $( "#pe_gal" ).animate({
+    height:"toggle"
+  }, 500, function() {});
+});
+$( "#st" ).click(function() {
+  $( "#st_gal" ).animate({
     height:"toggle"
   }, 500, function() {});
 });
 
 
+
+  $( "#port_gal" ).animate({
+    height:"toggle"
+  }, 5, function() {});
+  $( "#na_gal" ).animate({
+    height:"toggle"
+  }, 5, function() {});
+  $( "#pe_gal" ).animate({
+    height:"toggle"
+  }, 5, function() {});
+  $( "#st_gal" ).animate({
+    height:"toggle"
+  }, 5, function() {});
+
+
+$('img').click(function () {
+
+        var o=$(this).parent();
+
+        var url=o.find('img').attr('src');
+
+        var html='<a href="#" class="clone"><img src="'+url+'" /></a>'
+
+        o.append(html);
+
+        o=o.find('.clone');
+
+        o.animate({width:'200%',height:'200%'});
+
+        o.click(function () {
+
+            $(this).remove();
+
+        });
+
+    });
 
